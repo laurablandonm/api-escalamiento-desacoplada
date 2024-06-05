52d8a3954f6e99e8a3526c3f529c74b0b38bdddf
Laura Blandon <laurablandonm@gmail.com>
2024-06-04 19:22:47 -0500

    Adiciono balanceador y modifico el controller de actualizar del servicio de proyecto

--- api-escalamiento-proyecto-servicio/controllers/proyectoControllers.js
+++ api-escalamiento-proyecto-servicio/controllers/proyectoControllers.js
@@ -16,6 +16,7 @@ export const crearProyecto = async (requestProyecto) => {
 }
 
 export const listarProyecto = async () => {
+    console.log("estan haciendo una peticion...");
     const listaProyecto = await Proyecto.find().populate([
         {
             path:"cliente",
@@ -37,23 +38,20 @@ export const listarProyecto = async () => {
     return listaProyecto;
 }
 
-export const editarProyecto = async(requestProyecto, idProyecto) => {
-    const proyecto = await Proyecto.findById(idProyecto);
-    if(proyecto != null){
-        proyecto.numero = requestProyecto.numero;
-        proyecto.titulo = requestProyecto.titulo;
-        proyecto.fechaIniciacion = requestProyecto.fechaIniciacion;
-        proyecto.fechaEntrega = requestProyecto.fechaEntrega;
-        proyecto.cliente = requestProyecto.cliente.id;
-        proyecto.universidad = requestProyecto.universidad.id;
-        proyecto.tipoDeProyecto = requestProyecto.tipoDeProyecto.id;
-        proyecto.etapa = requestProyecto.etapa.id;
-        proyecto.fechaCreacion = new Date();
-        proyecto.fechaActualizacion = new Date();
-        const proyectoActualizado = await proyecto.save();
-        return proyectoActualizado;
-    } 
-    return null;
+export const editarProyecto = async({numero,titulo,fechaIniciacion,fechaEntrega,cliente,tipoDeProyecto,universidad,etapa}, idProyecto) => {
+    const proyectoRequest = {
+        numero,
+        titulo,
+        fechaIniciacion,
+        fechaEntrega,
+        cliente: cliente.id,
+        tipoDeProyecto: tipoDeProyecto.id,
+        universidad: universidad.id,
+        etapa: etapa.id
+    }
+    const proyecto = await Proyecto.findByIdAndUpdate(idProyecto, proyectoRequest, {new:true});
+    
+    return proyecto;
 }
 
 
